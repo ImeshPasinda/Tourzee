de6549cdf6a28cecc1582ec2cfd58195cbfab432
Imesh Pasinda <imeshpasinda@gmail.com>
2023-08-10 15:08:49 +0530

    Installed React-Scripts

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <div className="navbar">
       <div className="navContainer">
         <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
-          <span className="logo">lamabooking</span>
+          <span className="logo">tourzee</span>
         </Link>
         {user ? user.username : (
           <div className="navItems">
