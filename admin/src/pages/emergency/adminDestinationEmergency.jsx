35bca622054f24c266d468e73e2913cda4d236bf
Darshi Buddhini <darshibuddhini@gmail.com>
2023-10-15 13:06:00 +0530

    Update adminDestinationEmergency.jsx

--- admin/src/pages/emergency/adminDestinationEmergency.jsx
+++ admin/src/pages/emergency/adminDestinationEmergency.jsx
@@ -1,5 +1,4 @@
 
-
 import React, { useEffect, useState } from 'react';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
@@ -14,18 +13,20 @@ import {
   Paper,
   Typography,
   TextField,
-
 } from '@mui/material';
-import { Form, Input, Select, Switch, Upload, Button, Row, Col, Statistic, Modal, Popconfirm } from 'antd';
+import { Form, Input, Select, Button, Modal } from 'antd';
 
 function AdminDestinationEmergency() {
   const [safetyTips, setSafetyTips] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [form] = Form.useForm();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editingTip, setEditingTip] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get('http://localhost:8800/api/destinationsafetytips/'); // Replace with your actual API URL
+        const response = await axios.get('http://localhost:8800/api/destinationsafetytips/');
         if (response.status !== 200) {
           throw new Error('API request failed');
         }
@@ -37,56 +38,15 @@ function AdminDestinationEmergency() {
     fetchData();
   }, []);
 
-  const columns = [
-    { field: 'destination', headerName: 'Destination', flex: 1 },
-    { field: 'location', headerName: 'Location', flex: 1 },
-    { field: 'advicelevel', headerName: 'Advice Level', flex: 1 },
-    { field: 'timeperiod', headerName: 'Time Period', flex: 1 },
-    { field: 'edit', headerName: 'Edit', flex: 1 },
-    { field: 'delete', headerName: 'Delete', flex: 1 },
-  ];
-
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
 
-  const handleEditClick = (tipId) => {
-    // Implement your edit logic here
-    // You can navigate to an edit page or open a modal for editing
-    console.log(`Edit button clicked for tip with ID: ${tipId}`);
-  };
-
-  const handleDeleteClick = async (tipId) => {
-    // Implement your delete logic here
-    try {
-      // Make a DELETE request to your API to delete the safety tip
-      const response = await axios.delete(`http://localhost:8800/api/destinationsafetytips/${tipId}`); // Replace with your actual API URL
-      if (response.status === 200) {
-        // Remove the deleted safety tip from the local state
-        setSafetyTips((prevTips) => prevTips.filter((tip) => tip._id !== tipId));
-        console.log(`Safety tip with ID ${tipId} deleted successfully.`);
-      } else {
-        console.error(`Failed to delete safety tip with ID ${tipId}.`);
-      }
-    } catch (error) {
-      console.error('Error deleting safety tip:', error);
-    }
-  };
-
-  const filteredTips = safetyTips.filter((tip) =>
-    tip.destination.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
-  // ///////////////////////
-  const [form] = Form.useForm(); // Initialize Ant Design Form
-
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-
   const showAddModal = () => {
+    form.resetFields(); // Reset the form fields when opening the modal for adding
     setIsModalOpen(true);
   };
+
   const handleCancel = () => {
     setIsModalOpen(false);
   };
@@ -107,24 +67,16 @@ function AdminDestinationEmergency() {
         form.resetFields();
         setIsModalOpen(false);
 
+        // Refetch the data to update the UI after successful creation
+        // fetchData();
       } else {
         console.error('Failed to create safety tip.');
       }
     } catch (error) {
-      console.error('Error adding facility:', error);
+      console.error('Error adding safety tip:', error);
     }
   };
 
-  //////////////////////////////
-  const [editingTip, setEditingTip] = useState(null);
-
-  // Function to open the edit modal
-  const showEditModal = (tip) => {
-    setEditingTip(tip);
-    setIsModalOpen(true);
-  };
-
-  // Function to handle the edit save button click
   const handleEditSave = async () => {
     try {
       // Validate the form fields
@@ -143,13 +95,55 @@ function AdminDestinationEmergency() {
         setEditingTip(null);
 
         // Refetch the data to update the UI after successful update
+        // fetchData();
       } else {
         console.error('Failed to update safety tip.');
       }
     } catch (error) {
-      console.error('Error updating facility:', error);
+      console.error('Error updating safety tip:', error);
     }
   };
+
+  const columns = [
+    { field: 'destination', headerName: 'Destination', flex: 1 },
+    { field: 'location', headerName: 'Location', flex: 1 },
+    { field: 'advicelevel', headerName: 'Advice Level', flex: 1 },
+    { field: 'timeperiod', headerName: 'Time Period', flex: 1 },
+    { field: 'edit', headerName: 'Edit', flex: 1 },
+    { field: 'delete', headerName: 'Delete', flex: 1 },
+  ];
+  const showEditModal = (tip) => {
+    // Set the editingTip to the tip you want to edit
+    setEditingTip(tip);
+    form.setFieldsValue({
+      destination: tip.destination,
+      advicelevel: tip.advicelevel,
+      location: tip.location,
+      timeperiod: tip.timeperiod,
+    });
+    setIsModalOpen(true);
+  };
+  
+  const handleDeleteClick = async (tipId) => {
+    try {
+      // Make a DELETE request to your API to delete the safety tip
+      const response = await axios.delete(`http://localhost:8800/api/destinationsafetytips/${tipId}`);
+
+      if (response.status === 200) {
+        // Remove the deleted safety tip from the local state
+        setSafetyTips((prevTips) => prevTips.filter((tip) => tip._id !== tipId));
+        console.log(`Safety tip with ID ${tipId} deleted successfully.`);
+      } else {
+        console.error(`Failed to delete safety tip with ID ${tipId}.`);
+      }
+    } catch (error) {
+      console.error('Error deleting safety tip:', error);
+    }
+  };
+
+  const filteredTips = safetyTips.filter((tip) =>
+    tip.destination.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <div className="new">
@@ -158,18 +152,14 @@ function AdminDestinationEmergency() {
         <Navbar />
         <div className="safety-tips">
           <Typography variant="h5" gutterBottom>
-            <br></br>
             Destination Advice Level Management
           </Typography>
-          <br></br>
           <TextField
             label="Search Destination"
             variant="outlined"
             onChange={handleSearchChange}
             value={searchQuery}
           />
-          <br></br>
-          <br></br>
           <TableContainer component={Paper} style={{ maxWidth: '1200px' }}>
             <Table>
               <TableHead>
@@ -188,7 +178,7 @@ function AdminDestinationEmergency() {
                           <Button
                             variant="outlined"
                             color="primary"
-                            onClick={() => showEditModal(tip)} // Pass the tip to the edit modal
+                             onClick={() => showEditModal(tip)}
                           >
                             Edit
                           </Button>
@@ -211,15 +201,13 @@ function AdminDestinationEmergency() {
             </Table>
           </TableContainer>
 
-
-
-          {/* modal */}
           <div className="button-container">
             <Button type="primary" onClick={showAddModal}>
               Add Advice Levels
             </Button>
           </div>
-          <Modal visible={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+
+          <Modal visible={isModalOpen} onOk={editingTip ? handleEditSave : handleOk} onCancel={handleCancel}>
             <Form form={form} layout="vertical">
               <Form.Item name="destination" label="Destination" rules={[{ required: true, message: 'Please enter Name' }]}>
                 <Input />
@@ -230,58 +218,12 @@ function AdminDestinationEmergency() {
                   <Select.Option value="Exercise Normal Safety Precautions">Exercise Normal Safety Precautions</Select.Option>
                   <Select.Option value="Reconsider Your Need to Travel">Reconsider Your Need to Travel</Select.Option>
                   <Select.Option value="Do Not Travel">Do Not Travel</Select.Option>
-
                 </Select>
               </Form.Item>
               <Form.Item name="location" label="Location" rules={[{ required: true, message: 'Please enter City' }]}>
                 <Input />
               </Form.Item>
               <Form.Item name="timeperiod" label="Time Period" rules={[{ required: true, message: 'Please enter Address' }]}>
-                <Input />
-              </Form.Item>
-
-            </Form>
-          </Modal>
-
-
-          {/* Edit modal */}
-          <Modal visible={isModalOpen} onOk={handleEditSave} onCancel={handleCancel}>
-            <Form form={form} layout="vertical">
-              <Form.Item
-                name="destination"
-                label="Destination"
-                rules={[{ required: true, message: 'Please enter Name' }]}
-                initialValue={editingTip ? editingTip.destination : ''}
-              >
-                <Input />
-              </Form.Item>
-              <Form.Item
-                name="advicelevel"
-                label="Advice Level"
-                rules={[{ required: true, message: 'Please select Type' }]}
-                initialValue={editingTip ? editingTip.advicelevel : undefined}
-              >
-                <Select>
-                  <Select.Option value="Exercise a High Degree of Caution">Exercise a High Degree of Caution</Select.Option>
-                  <Select.Option value="Exercise Normal Safety Precautions">Exercise Normal Safety Precautions</Select.Option>
-                  <Select.Option value="Reconsider Your Need to Travel">Reconsider Your Need to Travel</Select.Option>
-                  <Select.Option value="Do Not Travel">Do Not Travel</Select.Option>
-                </Select>
-              </Form.Item>
-              <Form.Item
-                name="location"
-                label="Location"
-                rules={[{ required: true, message: 'Please enter City' }]}
-                initialValue={editingTip ? editingTip.location : ''}
-              >
-                <Input />
-              </Form.Item>
-              <Form.Item
-                name="timeperiod"
-                label="Time Period"
-                rules={[{ required: true, message: 'Please enter Address' }]}
-                initialValue={editingTip ? editingTip.timeperiod : ''}
-              >
                 <Input />
               </Form.Item>
             </Form>
